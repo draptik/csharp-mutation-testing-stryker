4d9b11be68d54e3cc28e16cfebeb6233cd691a81
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-02-01 21:06:49 +0100

    code: commented most inline data to show the power of mutation tests

--- code/Demo.Tests/RulesTests.cs
+++ code/Demo.Tests/RulesTests.cs
@@ -17,20 +17,20 @@ namespace Demo.Tests
         // Include each `InlineData` line one by one, run `dotnet stryker` and see how it changes the outcome.
         [Theory]
         [InlineData("a", true)]             // -> 3 survive
-        [InlineData("", false)]             // -> 2 survive
-        [InlineData(null, false)]           // -> 2 survive
-        [InlineData("123", true)]           // -> 1 survives
-        [InlineData("1234", false)]         // -> 0 survive
+        // [InlineData("", false)]             // -> 2 survive
+        // [InlineData(null, false)]           // -> 2 survive
+        // [InlineData("123", true)]           // -> 1 survives
+        // [InlineData("1234", false)]         // -> 0 survive
         public void ValidationRule_works(string input, bool expected) => 
             sut.Validate(input).Should().Be(expected);
 
         [Theory]
         [InlineData(5, 5, true)]    // addition
-        [InlineData(15, 5, true)]   // subtraction
-        [InlineData(2, 5, true)]    // multiplication
-        [InlineData(20, 2, true)]   // division
-        [InlineData(5, 4, true)]    // modulo
-        [InlineData(1, 1, false)]   // other combinations
+        // [InlineData(15, 5, true)]   // subtraction
+        // [InlineData(2, 5, true)]    // multiplication
+        // [InlineData(20, 2, true)]   // division
+        // [InlineData(5, 4, true)]    // modulo
+        // [InlineData(1, 1, false)]   // other combinations
         public void AnotherValidation_works(int a, int b, bool expected) =>
             sut.AnotherValidation(a, b).Should().Be(expected);
 
@@ -41,15 +41,16 @@ namespace Demo.Tests
         
         [Theory]
         [InlineData(true, true, true)]
-        [InlineData(true, false, true)]
-        [InlineData(false, true, true)]
-        [InlineData(false, false, false)]
+        // [InlineData(true, false, true)]
+        // [InlineData(false, true, true)]
+        // [InlineData(false, false, false)]
         public void Logical_works(bool a, bool b, bool expected) =>
             sut.Logical(a, b).Should().Be(expected);
 
+        // https://github.com/stryker-mutator/stryker-handbook/blob/master/mutator-types.md#method-expression
         [Theory]
-        [MemberData(nameof(GetFirstData), 1)]
-        // [MemberData(nameof(GetLinq1Data), 2)]
+        [MemberData(nameof(GetFirstData), 1)]   // list with length 1: First() == Last()
+        // [MemberData(nameof(GetFirstData), 2)]   // list with 2 different entries
         public void GetFirst_works(IEnumerable<int> collection, int expected) =>
             sut.GetFirst(collection).Should().Be(expected);
 
